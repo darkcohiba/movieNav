c0b1530574063200391d2ba99f763f8be3bfcf84
Sam <samuel.waters@gmail.com>
2022-10-27 11:27:14 -0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import movies from "./components/movies";
 
-function App() {
+export default function App() {
+  console.log(movies)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Movie Navigator</h1>
+      <section id="movies" className="grid"></section>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/movies.js
@@ -0,0 +1,163 @@
+export default [
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625360/intro%20to%20react%20workshop/baby-driver-rory-hi-res_jvwk2b.jpg",
+      name: "Baby Driver",
+      releaseYear: "2017",
+      website: "https://www.sonypictures.com/movies/babydriver",
+      genre: "Action"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625401/intro%20to%20react%20workshop/black-panther-web_kq4cxx.jpg",
+      name: "Black Panther",
+      releaseYear: "2018",
+      website: "https://www.marvel.com/movies/black-panther",
+      genre: "Action"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625428/intro%20to%20react%20workshop/il_794xN.3505829267_3ap2_b23n6a.jpg",
+      name: "Elf",
+      releaseYear: "2003",
+      website: "https://www.warnerbros.com/movies/elf",
+      genre: "Comedy"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625457/intro%20to%20react%20workshop/The-Lord-of-the-Rings-The-Return-of-the-King-Vintage-Movie-Poster-Original-1-Sheet-27x41_dc5e03aa-c6d1-4c48-b9a9-ce716488f259_800x.progressive_sxjggz.jpg",
+      name: "The Lord of the Rings - Return of the King",
+      releaseYear: "2003",
+      website: "https://www.warnerbros.com/movies/lord-rings-return-king",
+      genre: "Fantasy"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625491/intro%20to%20react%20workshop/f89U3ADr1oiB1s9GkdPOEpXUk5H_w5dbiy.jpg",
+      name: "The Matrix",
+      releaseYear: "1999",
+      website: "https://www.warnerbros.com/movies/matrix",
+      genre: "Sci-Fi"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625519/intro%20to%20react%20workshop/81zHy_InA5L._AC_SY679__ddkmqq.jpg",
+      name: "The Notebook",
+      releaseYear: "2004",
+      website: "https://www.warnerbros.com/movies/notebook",
+      genre: "Romance"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625598/intro%20to%20react%20workshop/71aYFk1gUqL._AC_SY679__ozld6j.jpg",
+      name: "Wedding Crashers",
+      releaseYear: "2005",
+      website: "https://www.warnerbros.com/movies/wedding-crashers",
+      genre: "Comedy"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625628/intro%20to%20react%20workshop/68511f8b-698b-4b1d-819b-e119a7abf12d_n85eth.jpg",
+      name: "Forgetting Sarah Marshall",
+      releaseYear: "2008",
+      website: "https://www.imdb.com/title/tt0800039/",
+      genre: "Comedy"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625659/intro%20to%20react%20workshop/MV5BMTg2MjkwMTM0NF5BMl5BanBnXkFtZTcwMzc4NDg2NQ_._V1__zoix8b.jpg",
+      name: "Crazy Stupid Love",
+      releaseYear: "2011",
+      website: "https://www.warnerbros.com/movies/crazy-stupid-love",
+      genre: "Comedy"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625687/intro%20to%20react%20workshop/avengers-endgame-journey-s-end-i122136_qbaopk.jpg",
+      name: "Avengers: Endgame",
+      releaseYear: "2019",
+      website: "https://www.marvel.com/movies/avengers-endgame",
+      genre: "Action"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625717/intro%20to%20react%20workshop/MV5BMTUxNzc0OTIxMV5BMl5BanBnXkFtZTgwNDI3NzU2NDE_._V1_FMjpg_UX1000__scldfh.jpg",
+      name: "Ex Machina",
+      releaseYear: "2014",
+      website: "https://a24films.com/films/ex-machina",
+      genre: "Sci-Fi"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625834/intro%20to%20react%20workshop/91UjyAWXgpL._RI__gfxaot.jpg",
+      name: "Ready Player One",
+      releaseYear: "2018",
+      website: "https://www.warnerbros.com/movies/ready-player-one",
+      genre: "Sci-Fi"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625871/intro%20to%20react%20workshop/p_800x1200_AMC_LongShot_06242019_x1lrs3.jpg",
+      name: "Long Shot",
+      releaseYear: "2019",
+      website: "https://www.lionsgate.com/movies/long-shot",
+      genre: "Comedy"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625906/intro%20to%20react%20workshop/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw_._V1__xrar6i.jpg",
+      name: "Inception",
+      releaseYear: "2010",
+      website: "https://www.warnerbros.com/movies/inception",
+      genre: "Thriller"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625945/intro%20to%20react%20workshop/HP2526_9bcd525a-6918-4e1f-b97c-760abd83189d_1024x1024_lhjqiz.jpg",
+      name: "Memento",
+      releaseYear: "2000",
+      website: "http://otnemem.com/index.html",
+      genre: "Thriller"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651625994/intro%20to%20react%20workshop/MV5BMjAyOTMyMzUxNl5BMl5BanBnXkFtZTcwODI4MzE0NA_._V1_FMjpg_UX1000__lvljgo.jpg",
+      name: "Bridesmaids",
+      releaseYear: "2011",
+      website: "https://www.imdb.com/title/tt1478338/",
+      genre: "Comedy"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651626072/intro%20to%20react%20workshop/71Wy1wq7ceL._AC_SL1099__k20ydt.jpg",
+      name: "The Fate of the Furious",
+      releaseYear: "2017",
+      website: "https://www.thefastsaga.com/fast-saga/ff8",
+      genre: "Action"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651626179/intro%20to%20react%20workshop/oFBQUufCV4c8WZoNhtGM17p884H_o9omb5.jpg",
+      name: "Pitch Perfect 2",
+      releaseYear: "2015",
+      website: "https://www.uphe.com/movies/pitch-perfect-2",
+      genre: "Comedy"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651626222/intro%20to%20react%20workshop/MV5BMjM2MTIxYTAtYWM3MC00OWI4LWJmMjMtYzBhMWFlMzU0NTdhXkEyXkFqcGdeQXVyMTkxNjUyNQ_._V1__oegymy.jpg",
+      name: "Noelle",
+      releaseYear: "2019",
+      website: "https://disneyplusoriginals.disney.com/movie/noelle",
+      genre: "Comedy"
+    },
+    {
+      posterUrl:
+        "https://res.cloudinary.com/dlzuobe8h/image/upload/v1651626259/intro%20to%20react%20workshop/MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM_._V1__c0hxph.jpg",
+      name: "Aquaman",
+      releaseYear: "2018",
+      website: "https://www.warnerbros.com/movies/aquaman",
+      genre: "Action"
+    }
+  ];
+  
